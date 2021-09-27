1397e454c87a4ad36798820c2b8f3a79393e4ba3
Ben Claudy <sharmaashu.2902@gmail.com>
2021-09-27 23:57:11 +0530

    map woking

--- client/src/Screens/HomeScreen/HomeScreen.js
+++ client/src/Screens/HomeScreen/HomeScreen.js
@@ -1,9 +1,21 @@
 import React from 'react'
-import { View, Text } from 'react-native';
+import { View, Text,StyleSheet } from 'react-native';
+import MapView,{PROVIDER_GOOGLE} from 'react-native-maps'
 const HomeScreen = () => {
     return (
-            <View>
+            <View style={{flex:1,height:"100%",width:"100%"}}>
                 <Text>HomeScreen</Text>
+                <MapView
+       provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+       style={{...StyleSheet.absoluteFill}}
+       region={{
+         latitude: 37.78825,
+         longitude: -122.4324,
+         latitudeDelta: 0.015,
+         longitudeDelta: 0.0121,
+       }}
+     >
+     </MapView>
             </View>
     )
 }
