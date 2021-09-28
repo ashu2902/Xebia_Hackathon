cf67d787ac1964006735fb3613db53be2b1df55c
Ben Claudy <sharmaashu.2902@gmail.com>
2021-09-28 06:42:04 +0530

    correctins

--- image-processing/main.py
+++ image-processing/main.py
@@ -131,67 +131,66 @@ while (cap.isOpened()):
                 break
 
 # Background Subtraction
-    frame_blur = cv2.GaussianBlur(frame.copy(), (5,5), 3)
-    # frame_blur = frame_blur[150:1000, 100:1800]
-    frame_gray = cv2.cvtColor(frame_blur, cv2.COLOR_BGR2GRAY)
-    frame_out = frame.copy()
+        frame_blur = cv2.GaussianBlur(frame.copy(), (5,5), 3)
+        # frame_blur = frame_blur[150:1000, 100:1800]
+        frame_gray = cv2.cvtColor(frame_blur, cv2.COLOR_BGR2GRAY)
+        frame_out = frame.copy()
 
     # Drawing the Overlay. Text at top left of video
-    if dict['text_overlay']:
-        str_on_frame = "%d/%d" % (video_cur_frame, video_info['num_of_frames'])
-        cv2.putText(frame_out, str_on_frame, (5,30), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.8, (0,255,255), 2, cv2.LINE_AA)
-        cv2.putText(frame_out,global_str + str(round(change_pos,2)) + 'sec', (5, 60), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.8, (255, 0, 0), 2, cv2.LINE_AA)
-        cv2.putText(frame_out,reqNumber,(5,90), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,255), 2, cv2.LINE_AA)
+        if dict['text_overlay']:
+                str_on_frame = "%d/%d" % (video_cur_frame, video_info['num_of_frames'])
+                cv2.putText(frame_out, str_on_frame, (5,30), cv2.FONT_HERSHEY_SIMPLEX,
+                                0.8, (0,255,255), 2, cv2.LINE_AA)
+                cv2.putText(frame_out,global_str + str(round(change_pos,2)) + 'sec', (5, 60), cv2.FONT_HERSHEY_SIMPLEX,
+                                0.8, (255, 0, 0), 2, cv2.LINE_AA)
+                cv2.putText(frame_out,reqNumber,(5,90), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,255), 2, cv2.LINE_AA)
         
     # motion detection for all objects in video
-    if dict['motion_detection']:
-        # frame_blur = frame_blur[380:420, 240:470]
-        # cv2.imshow('dss', frame_blur)
-        fgmask = fgbg.apply(frame_blur)
-        bw = np.uint8(fgmask==255)*255
-        bw = cv2.erode(bw, kernel_erode, iterations=1)
-        bw = cv2.dilate(bw, kernel_dilate, iterations=1)
-        # cv2.imshow('dss',bw)
-        # cv2.imwrite("frame%d.jpg" % co, bw)
-        (cnts, _) = cv2.findContours(bw.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        # loop over the contours
-        for c in cnts:
+        if dict['motion_detection']:
+       
+                fgmask = fgbg.apply(frame_blur)
+                bw = np.uint8(fgmask==255)*255
+                bw = cv2.erode(bw, kernel_erode, iterations=1)
+                bw = cv2.dilate(bw, kernel_dilate, iterations=1)
+                # cv2.imshow('dss',bw)
+                # cv2.imwrite("frame%d.jpg" % co, bw)
+                (cnts, _) = cv2.findContours(bw.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                # loop over the contours
+                for c in cnts:
             # print(cv2.contourArea(c))
             # if the contour is too small, we ignore it
-            if cv2.contourArea(c) < dict['min_area_motion_contour']:
-                continue
-            (x, y, w, h) = cv2.boundingRect(c)
-            cv2.rectangle(frame_out, (x, y), (x + w, y + h), (255, 0, 0), 1)
+                        if cv2.contourArea(c) < dict['min_area_motion_contour']:
+                                continue
+                        (x, y, w, h) = cv2.boundingRect(c)
+                        cv2.rectangle(frame_out, (x, y), (x + w, y + h), (255, 0, 0), 1)
             
- # detecting cars and vacant spaces
-    if dict['parking_detection']:
-        for ind, park in enumerate(parking_data):
-            points = np.array(park['points'])
-            rect = parking_bounding_rects[ind]
-            roi_gray = frame_gray[rect[1]:(rect[1]+rect[3]), rect[0]:(rect[0]+rect[2])] 
+        # detecting cars and vacant spaces
+        if dict['parking_detection']:
+                for ind, park in enumerate(parking_data):
+                        points = np.array(park['points'])
+                        rect = parking_bounding_rects[ind]
+                        roi_gray = frame_gray[rect[1]:(rect[1]+rect[3]), rect[0]:(rect[0]+rect[2])] 
 
-            laplacian = cv2.Laplacian(roi_gray, cv2.CV_64F)
-            # cv2.imshow('oir', laplacian)
-            points[:,0] = points[:,0] - rect[0] 
-            points[:,1] = points[:,1] - rect[1]
-            delta = np.mean(np.abs(laplacian * parking_mask[ind]))
+                        laplacian = cv2.Laplacian(roi_gray, cv2.CV_64F)
+                        # cv2.imshow('oir', laplacian)
+                         points[:,0] = points[:,0] - rect[0] 
+                        points[:,1] = points[:,1] - rect[1]
+                        delta = np.mean(np.abs(laplacian * parking_mask[ind]))
           
-            status = delta < dict['park_laplacian_th']
-            # If detected a change in parking status, save the current time
-            if status != parking_status[ind] and parking_buffer[ind]==None:
-                parking_buffer[ind] = video_cur_pos
-                change_pos = video_cur_pos
+                        status = delta < dict['park_laplacian_th']
+                        # If detected a change in parking status, save the current time
+                        if status != parking_status[ind] and parking_buffer[ind]==None:
+                                parking_buffer[ind] = video_cur_pos
+                                change_pos = video_cur_pos
                
-            # If status is still different than the one saved and counter is open
-            elif status != parking_status[ind] and parking_buffer[ind]!=None:
-                if video_cur_pos - parking_buffer[ind] > dict['park_sec_to_wait']:
-                    parking_status[ind] = status
-                    parking_buffer[ind] = None
-            # If status is still same and counter is open
-            elif status == parking_status[ind] and parking_buffer[ind]!=None:
-                parking_buffer[ind] = None
+                         # If status is still different than the one saved and counter is open
+                        elif status != parking_status[ind] and parking_buffer[ind]!=None:
+                                if video_cur_pos - parking_buffer[ind] > dict['park_sec_to_wait']:
+                                        parking_status[ind] = status
+                                        parking_buffer[ind] = None
+                         # If status is still same and counter is open
+                        elif status == parking_status[ind] and parking_buffer[ind]!=None:
+                                parking_buffer[ind] = None
                 
 # counting Vacant Spaces
 for a, b in enumerate(parking_data):
@@ -228,9 +227,83 @@ for a, b in enumerate(parking_data):
                     totalNum = [park.values()]
 #                     print('hello', totalNum)
                     countArray.append(totalNum);
-                    
 totalNum = len(countArray)       
 print('Total Counts',totalNum)
+
+  if parking_data_motion != []:
+        for index, park_coord in enumerate(parking_data_motion):
+            points = np.array(park_coord['points'])
+            color = (0, 0, 255)
+            recta = parking_bounding_rects[ind]
+            roi_gray1 = frame_gray[recta[1]:(recta[1] + recta[3]),
+                            recta[0]:(recta[0] + recta[2])]  # crop roi for faster calcluation
+           
+            
+            fgbg1 = cv2.createBackgroundSubtractorMOG2(history=300, varThreshold=16, detectShadows=True)
+            roi_gray1_blur = cv2.GaussianBlur(roi_gray1.copy(), (5, 5), 3)
+            fgmask1 = fgbg1.apply(roi_gray1_blur)
+            bw1 = np.uint8(fgmask1 == 255) * 255
+            bw1 = cv2.erode(bw1, kernel_erode, iterations=1)
+            bw1 = cv2.dilate(bw1, kernel_dilate, iterations=1)
+            (cnts1, _) = cv2.findContours(bw1.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            # loop over the contours
+            for c in cnts1:
+                print(cv2.contourArea(c))
+                # if the contour is too small, we ignore it
+                if cv2.contourArea(c) < 4:
+                    continue
+                (x, y, w, h) = cv2.boundingRect(c)
+                classifier_result1 = run_classifier(roi_gray1, index)
+                if classifier_result1:
+                # print(classifier_result)
+                    color = (0, 0, 255)  # Red again if car found by classifier
+                    
+                else:
+                    color = (0,255, 0)
+            classifier_result1 = run_classifier(roi_gray1, index)
+            if classifier_result1:
+                # print(classifier_result)
+                color = (0, 0, 255)  # Red again if car found by classifier
+            else:
+                color = (0, 255, 0)
+            cv2.drawContours(frame_out, [points], contourIdx=-1,
+                                 color=color, thickness=2, lineType=cv2.LINE_8)
+            
+            
+        if dict['pedestrian_detection']:
+        # detect people in the image. Slows down the program, requires high GPU speed
+        (rects, weights) = hog.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.05)
+        # draw the  bounding boxes
+        for (x, y, w, h) in rects:
+            cv2.rectangle(frame_out, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            
+         # write the output frames
+    if dict['save_video']:
+#         if video_cur_frame % 35 == 0: # take every 30 frames
+            out.write(frame_out)
+    # Display video
+    cv2.imshow('frame', frame_out)
+    # cv2.imshow('background mask', bw)
+    k = cv2.waitKey(1)
+    if k == ord('q'):
+        break
+    elif k == ord('c'):
+        cv2.imwrite('frame%d.jpg' % video_cur_frame, frame_out)
+    elif k == ord('j'):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, video_cur_frame+1000) # jump 1000 frames
+    elif k == ord('u'):
+        cap.set(cv2.CAP_PROP_POS_FRAMES, video_cur_frame + 500)  # jump 500 frames
+    if cv2.waitKey(33) == 27:
+        break
+        
+    count = 0
+    
+cv2.waitKey(0)
+cap.release()
+if dict['save_video']: out.release()
+cv2.destroyAllWindows()   
+
+
 
 
 
